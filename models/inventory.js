9a05dfe8603d20692b16bf702e30c277c6679585
chadbanghart <cbanghar@gmail.com>
2024-04-19 14:36:45 -0700

    create inventory and inventoryItem Schemas

---
+++ models/inventory.js
@@ -0,0 +1,48 @@
+const mongoose = require('mongoose');
+const Schema = mongoose.Schema;
+
+const inventoryItemSchema = new Schema({
+  user: {
+    type: Schema.Types.ObjectId,
+    ref: 'User',
+    required: true
+  },
+  quantity: {
+    type: Number,
+    min: 0
+  },
+  unit: {
+    type: String
+  },
+  item: {
+    type: Schema.Types.ObjectId,
+    ref: 'Item',
+    required: true
+  },
+  dateOfExpire: {
+    type: Date
+  },
+  location: {
+    type: String,
+    enum: ['Fridge', 'Freezer', 'Pantry']
+  },
+  favItem: {
+    type: Boolean
+  } 
+}, {
+  timestamps: true
+})
+
+const inventorySchema = new Schema({
+  user: {
+    type: Schema.Types.ObjectId,
+    ref: 'User',
+    required: true
+  },
+  inventoryItem: [inventoryItem]
+},
+{
+  timestamps: true
+});
+
+module.exports = mongoose.model('Inventory', inventorySchema);
